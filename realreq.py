55f94a2f98b7034fbb5c6905848cfd97d5e0dd65
Tyler Calder <calder.ty@gmail.com>
2020-12-31 18:43:06 -0700

    Add code fore realreq

---
+++ realreq.py
@@ -0,0 +1,125 @@
+"""Real Requirements
+
+Parses through the list of installed packages and explores your source code to
+identify what the real requirements of your software are.
+"""
+import argparse
+import pathlib
+import re
+import subprocess
+
+IMPORT_RE = re.compile(
+    r"(from )?(?(1)(?P<from>[a-zA-Z0-9._]*)|import (?P<import>[a-zA-Z0-9+._]*))"
+)
+
+
+def main():
+    app = Application()
+    app()
+
+
+class Application:
+    """Main Application
+
+    This will be a CLI tool used to gather information about the requirments
+    actually used in your source code
+    """
+
+    def __init__(self):
+        self.parser = argparse.ArgumentParser()
+        self.parser.add_argument("-s", "--source", default=".")
+
+    def __call__(self):
+        # Gather imports
+        # Find dependencies
+        # Find Dependency versions
+        args = self.parser.parse_args()
+        pkgs = search_source(args.source)
+        deps = build_dep_list(pkgs)
+        dep_ver = _get_dependency_versions(deps)
+        print("\n".join(["{0}=={1}".format(k, v) for k, v in dep_ver.items()]))
+
+
+def search_source(source):
+    """Go through the source directory and identify all modules"""
+    source_files = list(pathlib.Path(source).rglob("*.[Pp][Yy]"))
+
+    imports = []
+    for file_ in source_files:
+        with file_.open() as f:
+            lines = f.readlines()
+        for line in lines:
+            module = _scan_for_imports(line)
+            if module:
+                imports.append(module)
+
+    # Now we want to clean out the imports that we have
+    # 1. Eliminate the imports which start with `.` These are relative
+    #   imports.
+    # 2. Split imports on `.` we only want the top level module name
+    # 3. Remove imports whose name begins with the same name as `source` these
+    #   are local modules, not modules being installed from pip
+    imports = [m for m in imports if not m.startswith(".")]
+    imports = [m.split(".")[0] for m in imports]
+    imports = set(imports)
+    imports.discard(source)
+    return imports
+
+
+def _scan_for_imports(line):
+    """Scans line for import syntax, returning the name of the module imported"""
+    lm = IMPORT_RE.match(line)
+    if lm:
+        module = (
+            lm.groupdict()["from"]
+            if lm.groupdict()["from"]
+            else lm.groupdict()["import"]
+        )
+        return module
+    else:
+        return ""
+
+
+def build_dep_list(pkgs):
+    errs = []
+    pkgs_ = list(pkgs)
+    dependencies = {}
+
+    while pkgs_:
+        pkg = pkgs_.pop()
+        if pkg in dependencies:
+            # We've seen this already, lets not duplicate it
+            continue
+        try:
+            results = subprocess.run(
+                ["pip", "show", pkg], stdout=subprocess.PIPE, check=True
+            )
+        except Exception:
+            errs.append(pkg)
+            continue
+
+        out_text = results.stdout.decode("utf-8").split("\n")
+        for line in out_text:
+            if line.startswith("Requires"):
+                # Requires: is 9 chars long
+                p = line[9:].rstrip().split(",")
+                p = [_.strip() for _ in p]
+                dependencies[pkg] = p
+                # Add in the found dependencies and merge it in!
+                pkgs_.extend(p)
+    return list(dependencies.keys())
+
+
+def _get_dependency_versions(dependencies):
+    results = subprocess.run(["pip", "freeze"], stdout=subprocess.PIPE, check=True)
+    out_text = results.stdout.decode("utf-8").strip().split("\n")
+    dep_ver = {}
+    for line in out_text:
+        dep, ver = line.split("==")
+        if dep in dependencies:
+            dep_ver[dep] = ver
+    return dep_ver
+
+
+if __name__ == "__main__":
+    main()
---
+++ test_realreq.py
@@ -0,0 +1,116 @@
+# Copyright 2020 Tyler Calder
+
+# Design my Requirements
+# I want to be able to search all source code to find used packages
+# in the code
+# I then want to take the ones found and chek in the list of installed
+# and then check their dependencies and then write
+# packages and send them to the std out
+import os
+import unittest.mock
+import sys
+
+import pytest
+from pytest_mock import mocker
+
+
+import realreq
+
+CONTENT = """
+import requests
+from foo import bar
+from . import local_module
+import local_module2
+from foo.baz import frum
+"""
+
+_MOCK_DEPENDENCY_TREE = {
+    "foo": ["bar"],
+    "requests": ["baz", "spam"],
+    "baz": [],
+    "spam": ["egg", "wheel"],
+    "egg": ["pip"],
+    "pip": [],
+    "wheel": [],
+}
+
+_MOCK_DEP_VERSIONS = {
+    "foo": "1.0.0",
+    "baz": "0.1.0",
+    "spam": "3.2.12",
+    "egg": "13.0",
+    "pip": "2.12.1",
+    "wheel": "1.1.1",
+    "notused": "201.10.1",
+    "DevDep": "0.1.1",
+    "testDep": "0.1.3",
+}
+
+
+def mock_pip_show(*args, **kwargs):
+    pkg = args[0][2]
+    deps = _MOCK_DEPENDENCY_TREE[pkg]
+    mock_result = unittest.mock.MagicMock()
+    mock_result.configure_mock(
+        **{
+            "stdout": "stuff\nRequires: {0}\nmore stuff".format(
+                ", ".join(deps)
+            ).encode()
+        }
+    )
+
+    return mock_result
+
+
+def mock_pip_freeze(*args, **kwargs):
+    result = b"\n".join(
+        ["{0}=={1}".format(k, v).encode() for k, v in _MOCK_DEP_VERSIONS.items()]
+    )
+    mock_result = unittest.mock.MagicMock()
+    mock_result.configure_mock(**{"stdout": result})
+    return mock_result
+
+
+@pytest.fixture(scope="session")
+def source_files(tmp_path_factory):
+    src = tmp_path_factory.mktemp("src")
+    main = src / "main.py"
+    main.write_text(CONTENT)
+    return src
+
+
+def test_search_source_for_used_packages(source_files):
+    """Source code is searched and aquires the name of all packages used"""
+    pkgs = realreq.search_source(str(source_files))
+    assert all([_ in pkgs for _ in ["requests", "foo", "local_module2"]])
+
+
+def test_build_dependency_list(mocker):
+    """Dependency Tree build out should identify all the dependencies a module has"""
+    # Essentially we want to make sure that the values returned from the system
+    # are what we woule get by running `pip show x`, and then getting the "Requires" value
+    mock_run = mocker.patch("subprocess.run")
+    mock_run.side_effect = mock_pip_show
+
+    pkgs = ["requests", "foo", "local_module2"]
+    dep_tree = realreq.build_dep_list(pkgs)
+    assert all([_ in dep_tree for _ in list(_MOCK_DEPENDENCY_TREE.keys())])
+
+
+def test_get_dependency_versions(mocker):
+    # Get list of dependencies, sift through list of installed packages
+    # and return in
+    mock_run = mocker.patch("subprocess.run")
+    mock_run.side_effect = mock_pip_freeze
+
+    pkgs = _MOCK_DEPENDENCY_TREE.keys()
+    versions = realreq._get_dependency_versions(pkgs)
+    assert versions == {
+        "foo": "1.0.0",
+        "baz": "0.1.0",
+        "spam": "3.2.12",
+        "egg": "13.0",
+        "pip": "2.12.1",
+        "wheel": "1.1.1",
+    }
+
