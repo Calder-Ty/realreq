6b9b6562a43dfbbe093d1460cb7541f72085e856
Tyler Calder <calder.ty@gmail.com>
2021-01-02 21:03:44 -0700

    Add tox and setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="realreq",
+    version="0.1.0",
+    author="Tyler Calder",
+    author_email="calder-ty@protonmail.com",
+    description="CLI tool to gather dependencies for imports actually used by your code",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+    ],
+    scripts=["realreq.py"],
+    python_requires=">=3.5",
+)
+
