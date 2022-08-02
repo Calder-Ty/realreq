4a53f6ace23d1b425cc55e5528f4fab48618e25b
Tyler Calder <calder-ty@protonmail.com>
2022-08-02 21:16:34 +0000

    docs: Clarify Python Supported Version

--- setup.py
+++ setup.py
@@ -13,7 +13,11 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/Calder-Ty/realreq",
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6"
+        "Programming Language :: Python :: 3.7"
+        "Programming Language :: Python :: 3.8"
+        "Programming Language :: Python :: 3.9"
+        "Programming Language :: Python :: 3.10"
         "Operating System :: POSIX :: Linux",
         "Development Status :: 4 - Beta",
         "License :: OSI Approved :: MIT License",
