bb6f89e0fba3916b2987226b39fbfc61e0f69496
Tyler Calder <calder-ty@protonmail.com>
2022-09-16 14:59:09 -0600

    feat: add dependency graph structure

---
+++ tests/test_dependency_graph.py
@@ -0,0 +1,14 @@
+"""Tests for the Dependency Graph"""
+import _realreq.requtils.dependency_tree as graph
+
+
+class TestDependencyGraph:
+    def test_add_dependency_works_for_empty_graph(self):
+        g = graph.DependencyGraph()
+        g.add_dependency("foo", "bar")
+        assert g.get_dependencies("bar") == {"foo"}
+
+    def test_add_node(self):
+        g = graph.DependencyGraph()
+        g.add_node("foo")
+        assert set() == g.get_dependencies("foo")
