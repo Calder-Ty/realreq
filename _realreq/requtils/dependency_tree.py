bac6ea9816e5ee52def230b146286eeda9f18a29
Tyler Calder <calder-ty@protonmail.com>
2022-09-20 15:06:39 -0600

    feat: speed up the inversion of a tree

--- _realreq/requtils/dependency_tree.py
+++ _realreq/requtils/dependency_tree.py
@@ -4,10 +4,17 @@ import typing
 
 class _Dependency:
     def __init__(
-        self, name: str, dependencies: typing.Optional[typing.List[str]] = None
+        self,
+        name: str,
+        dependencies: typing.Optional[typing.List[str]] = None,
+        dependants: typing.Optional[typing.List[str]] = None,
     ):
         self.name = name
         self.dependencies = set(dependencies) if dependencies is not None else set()
+        self.dependants = set(dependants) if dependants is not None else set()
+
+    def invert(self):
+        self.dependants, self.dependencies = self.dependencies, self.dependants
 
 
 class DependencyTree:
@@ -38,9 +45,11 @@ class DependencyGraph:
 
     def add_dependency(self, name: str, dependant: str):
         """Add the name as a dependency of the given dependant"""
-        _ = self._nodes.setdefault(name, _Dependency(name))
+        requirement = self._nodes.setdefault(name, _Dependency(name))
         dep = self._nodes.setdefault(dependant, _Dependency(dependant))
+
         dep.dependencies.add(name)
+        requirement.dependants.add(dependant)
 
     def get_dependencies(self, name) -> typing.Collection[str]:
         """Get a list of dependencies"""
@@ -55,10 +64,6 @@ class DependencyGraph:
 
     def invert(self):
         """Inverts the relationships in the graph"""
-        # TODO: We can make this faster
-        inverted_tree = DependencyGraph()
-        for pkg, deps in self._nodes.items():
-            inverted_tree.add_node(pkg)
-            for dep in deps.dependencies:
-                inverted_tree.add_dependency(pkg, dep)
-        return inverted_tree
+        for deps in self._nodes.values():
+            deps.invert()
+        return self
